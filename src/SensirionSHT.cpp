0e1f7121b00e4c6c832047e05856fd0f5c99b2c1
Sebastian Kraus <sekdiy@gmail.com>
2015-07-01 00:48:21 +0200

    fixed issue where humidity didn't update

--- src/SensirionSHT.cpp
+++ src/SensirionSHT.cpp
@@ -1,3 +1,11 @@
+/**
+ * Sensirion SHT library
+ *
+ * @author sekdiy
+ * @date 01.07.2015
+ * @version 2
+ */
+
 // Compatibility with the Arduino 1.0 library standard
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
@@ -5,49 +13,72 @@
 #include "WProgram.h"
 #endif
 
-#include "SensirionSHT.h"
+#include <SensirionSHT.h>
 
-SensirionSHT::SensirionSHT(unsigned int dataPin, unsigned int clockPin, float period): Sensirion(dataPin, clockPin) {
-  this->_period = period;
-  this->_duration = period;
+/**
+ *
+ */
+SensirionSHT::SensirionSHT(unsigned int dataPin, unsigned int clockPin, float period)
+  : Sensirion(dataPin, clockPin), _period(period), _duration(period) {
+}
+
+bool SensirionSHT::hasTemperature() {
+  return !isnan(this->_temperature);
 }
 
 float SensirionSHT::getTemperature() {
-  return this->_temperature;
+  return this->_temperature;                                    // return last temperature value
+}
+
+bool SensirionSHT::hasHumidity() {
+  return !isnan(this->_humidity);
 }
 
 float SensirionSHT::getHumidity() {
-  return this->_humidity;
+  return this->_humidity;                                       // return last humidity value
+}
+
+bool SensirionSHT::hasDewpoint() {
+  return this->hasTemperature() && this->hasHumidity();
 }
 
 float SensirionSHT::getDewpoint() {
-  return this->_dewpoint;
+  return this->calcDewpoint(this->_humidity, this->_temperature);   // return last dew point value
 }
 
-void SensirionSHT::tick(float duration) {
-  this->_duration += duration;
+bool SensirionSHT::isOutdated() {
+  return !this->_fresh;                                         // return iff results aren't fresh any more
+}
 
-  if (this->_period - this->_duration < 0.01) {
-    this->_duration = 0;
-    this->_active = false;     
+unsigned int SensirionSHT::tick(float duration) {
+  unsigned int error = 0;
+
+  this->_duration += duration;                                  // update duration inbetween measurements
+  this->_fresh = false;                                         // mark previous results as outdated
+
+  if (this->_period - this->_duration < 0.01) {                 // has the waiting duration reached the period length?
+    this->_ready = false;                                       // mark measurement cycle as started
+    this->_mode = TEMP;                                         // switch to temperature for next measurement
+    error = this->meas(this->_mode, &this->_data, NONBLOCK);    // initiate a new measurement
+    this->_duration = 0.0f;                                     // reset duration for next waiting period
   }
 
-  if (!this->_active) {
-    this->meas(this->_mode, &this->_data, NONBLOCK);
-    this->_active = true;
-  } 
-  else if (this->measRdy()) {
-    switch (this->_mode) {
-      case TEMP:
-          this->_temperature = this->calcTemp(this->_data);
-          this->_mode = HUMI;
+  if ((!this->_ready) && (S_Meas_Rdy == this->measRdy()))       // if measurement is running and sensor data becomes available
+  {
+    switch (this->_mode)                                        // only one kind of value can be transmitted at a given time
+    {
+      case TEMP:                                                // temperature measurement:
+        this->_temperature = this->calcTemp(this->_data);       // store temperature value
+        this->_mode = HUMI;                                     // switch to humidity for next measurement
+        error = this->meas(this->_mode, &this->_data, NONBLOCK);// initiate a new measurement
         break;
-      case HUMI:
-          this->_humidity = this->calcHumi(this->_data, this->_temperature);
-          this->_dewpoint = this->calcDewpoint(this->_humidity, this->_temperature);
-          this->_mode = TEMP;
+      case HUMI:                                                // humidity measurement
+        this->_humidity = this->calcHumi(this->_data, this->_temperature);            // store humidity value
+        this->_ready = true;                                    // mark measurement cycle as completed
+        this->_fresh = true;                                    // mark new results as fresh
         break;
     }
-  }  
-}
+  }
 
+  return error;
+}
--- src/SensirionSHT.h
+++ src/SensirionSHT.h
@@ -1,26 +1,42 @@
+/**
+ * Sensirion SHT library
+ *
+ * @author sekdiy
+ * @date 01.07.2015
+ * @version 2
+ */
+
+#ifndef SENSIRIONSHT_H
+#define SENSIRIONSHT_H
+
 #include <Sensirion.h>                // http://playground.arduino.cc/Code/Sensirion
 
-class SensirionSHT: public Sensirion 
+class SensirionSHT: public Sensirion
 {
   public:
     SensirionSHT(unsigned int dataPin, unsigned int clockPin, float period = 5.0f);
 
-    float getTemperature();
-    float getHumidity();
-    float getDewpoint();
+    bool hasTemperature();          // check for succesful temperature measurement cycle
+    bool hasHumidity();             // check for succesful humidity measurement cycle
+    bool hasDewpoint();             // check for succesful temperature and humidity measurement cycles
+    bool isOutdated();              // check if last measurement cycle was longer than one tick ago
 
-    void tick(float duration);
-    
+    float getTemperature();         // get calculated temperature
+    float getHumidity();            // get calculated humidity
+    float getDewpoint();            // get calculated dew point
+
+    unsigned int tick(float duration);  // update measurement
+
   protected:
-    float _period;
-    float _duration = 0.0f;
-    bool _active = false;
-    unsigned int _mode = TEMP;
-    unsigned int _data = 0;
+    float _period;                  // waiting period inbetween measurements
+    float _duration;                // time waited during current period
+    bool _fresh = false;            // measurement currently running
+    bool _ready = false;            // updated calculations available
+    unsigned int _mode;             // measurement mode (TEMP or HUMI)
+    unsigned int _data;             // raw measurement result
 
-    float _temperature = 1e-99f;
-    float _humidity = 1e-99f;
-    float _dewpoint = 1e-99f;
+    float _temperature = NAN;  // calculated temperature
+    float _humidity = NAN;     // calculated humidity
 };
 
-
+#endif   // SENSIRIONSHT_H
